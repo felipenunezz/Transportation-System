1f528d29e87ab1cef9fa7ce09c04eb426fcd5ca4
felipe <fellpe091104@gmail.com>
2026-07-10 17:16:24 +0300

    For now its all i need for BusTrackingHub.cs.

--- Transportation System/Hubs/BusTrackingHub.cs
+++ Transportation System/Hubs/BusTrackingHub.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Transportation_System.Models.Domain;
 
 namespace Transportation_System.Hubs;
 
 public class BusTrackingHub : Hub
 {
-
+ public async Task SendAsync(string Message, Bus bus)
+ {
+     await Clients.All.SendAsync("ReceiveMessage", Message, bus);
+ }
 }
